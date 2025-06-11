910d41e3ec3293e63f3326d8a4afe781433895d8
Armen90210 <150281639+Menchna@users.noreply.github.com>
2025-06-11 20:52:22 +0400

    Update str_capitalizer.c

--- level_3/str_capitalizer.c
+++ level_3/str_capitalizer.c
@@ -52,3 +52,43 @@ int main(int	argc, char	**argv)
 		write (1, "\n", 1);
 	return (0);
 }
+
+/*
+#include <unistd.h>
+
+int main(int	argc, char	**argv)
+{
+	int	i = 0;
+	int	j = 1;
+
+	if (argc > 1)
+	{
+		while (j < argc)
+		{
+			i = 0;
+			while (argv[j][i] != '\0')
+			{
+				if (argv[j][i] >= 'A' && argv[j][i] <= 'Z')
+					argv[j][i] += 32;
+				i++;
+			}
+			i = 0;
+			while (argv[j][i] != '\0')
+			{
+				if (argv[j][0] >= 'a' && argv[j][i] <= 'z')
+					argv[j][i] -= 32;
+				else if ((argv[j][i] >= 'a' && argv[j][i] <= 'z')
+					&& (argv[j][i - 1] == ' ' || argv[j][i - 1] == '\t'))
+					argv[j][i] -= 32;
+				write (1, &argv[j][i], 1);
+				i++;
+			}
+			write (1, "\n", 1);
+			j++;
+		}
+	}
+	else
+		write (1, "\n", 1);
+	return (0);
+}
+*/
